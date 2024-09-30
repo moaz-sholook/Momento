bc7ca4129a60eeda88f8293fa2b509b5444c8935
moaz-sholook <sholookmoaz@gmail.com>
2024-09-30 17:47:21 -0400

    Add files via upload

---
+++ backend/picture.py
@@ -0,0 +1,49 @@
+import cv2
+import time 
+import requests
+
+
+# Initialize the camera
+camera = cv2.VideoCapture(0)
+
+# Check if the camera opened successfully
+if not camera.isOpened():
+    print("Error: Could not open camera.")
+    exit()
+
+server_url = "https://momento-delta.vercel.app/upload"
+
+try:
+   while True:
+        for i in range(5):
+             camera.read()
+
+        # Read a single frame from the camera
+        ret, frame = camera.read()
+
+        if ret:
+            # Save the frame as an image file
+            timestamp = time.strftime("%Y%m%d-%H%M%S")
+            filename = f'webcam_picture_{timestamp}.jpg'
+            cv2.imwrite(filename, frame)
+            print(f"Picture saved as {filename}")
+
+            with open(filename, 'rb') as image_file:
+                response = requests.post(server_url,files={'image':image_file})
+
+            if response.status_code == 200:
+                 print("Image sent")
+            
+            else:
+                 print("Failed to send")
+        else:
+            print("Error: Could not capture frame.")
+
+        time.sleep(20)
+        
+        
+except KeyboardInterrupt:
+        print("program interrupted. Exiting...")
+
+finally:
+     camera.release()
